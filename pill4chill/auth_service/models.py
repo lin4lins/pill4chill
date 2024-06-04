3d6b5844ca5ef74c0a58f75f473e394cac6e51f0
Elina Shramko <elina.vl.shramko@gmail.com>
2024-06-04 12:34:34 +0300

    Apply black formatting

--- pill4chill/auth_service/models.py
+++ pill4chill/auth_service/models.py
@@ -5,4 +5,4 @@ from django.db import models
 class User(AbstractBaseUser):
     full_name = models.CharField()
     email = models.EmailField()
-    created_at = models.DateTimeField(auto_now_add = True)
+    created_at = models.DateTimeField(auto_now_add=True)
