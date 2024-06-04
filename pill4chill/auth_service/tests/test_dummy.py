138430b87c4165e10db5d7f2210ac23a38622b4d
Elina Shramko <elina.vl.shramko@gmail.com>
2024-06-04 15:16:00 +0300

    Add dummy test

--- pill4chill/auth_service/tests/test_dummy.py
+++ pill4chill/auth_service/tests/test_dummy.py
@@ -3,4 +3,4 @@ from rest_framework.test import APITestCase
 
 class DummyTestCase(APITestCase):
     def test_none(self):
-        self.assertEqual(True, True)
+        self.assertEqual(True, False)
